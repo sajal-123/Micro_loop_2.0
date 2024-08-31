37518ac85605c7e9f1b6288ffb2eb57d8e7522b3
sajal-123 <work.sajalgarg.1914@gmail.com>
2024-09-01 00:54:03 +0530

    Smal Change

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
-import { UserButton } from "@clerk/nextjs";
-
+"use client"; // Add this directive at the top
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/dashboard"); // Redirect to the dashboard page
+  }, [router]);
+
   return (
     <div>
       <h2>Hello brother</h2>
